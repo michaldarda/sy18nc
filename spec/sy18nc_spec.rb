a9d01a5d66b68639d2156f8041e97cff05e7bb67
Michał Darda <michaldarda@gmail.com>
2013-11-04 12:14:20 +0100

    It is called files now

--- spec/sy18nc_spec.rb
+++ spec/sy18nc_spec.rb
@@ -4,19 +4,19 @@ describe Sy18nc do
   it "is configurable" do
     Sy18nc.config.base_locale.must_equal "en"
     Sy18nc.config.locales_dir.must_equal "#{Dir.pwd}/config/locales"
-    Sy18nc.config.synchronizable.must_equal []
+    Sy18nc.config.files.must_equal []
     Sy18nc.config.locales.must_equal []
 
     Sy18nc.configure do |c|
       c.base_locale = "ru"
       c.locales_dir = "spec/fixtures"
-      c.synchronizable = ["code", "devise", "doorkeeper"]
+      c.files = ["code", "devise", "doorkeeper"]
       c.locales = ["en", "es", "fr", "de"]
     end
 
     Sy18nc.config.base_locale.must_equal "ru"
     Sy18nc.config.locales_dir.must_equal "spec/fixtures"
-    Sy18nc.config.synchronizable.must_equal ["code", "devise", "doorkeeper"]
+    Sy18nc.config.files.must_equal ["code", "devise", "doorkeeper"]
     Sy18nc.config.locales.must_equal ["en", "es", "fr", "de"]
   end
 end
